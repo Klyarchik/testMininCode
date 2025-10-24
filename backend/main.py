25191d5e8022acbd9b11549e48001550e2e31002
Klyarchik <zakvon79.104@gmail.com>
2025-10-24 10:24:41 +0300

    Add haynya kakayato

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,15 @@ from routers.auth_router import auth_router
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth_router)
 
 @app.exception_handler(MissingTokenError)
